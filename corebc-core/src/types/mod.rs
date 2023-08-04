96ec804c848c35741accaa9201c853b1de4eb215
Kuly14 <kulman.david@gmail.com>
2023-08-04 17:28:41 +0200

    Add U456 and H456 types

--- corebc-core/src/types/mod.rs
+++ corebc-core/src/types/mod.rs
@@ -8,7 +8,8 @@ pub type Selector = [u8; 4];
 pub use ethabi::ethereum_types::H256 as TxHash;
 
 pub use ethabi::ethereum_types::{
-    Address, BigEndianHash, Bloom, H128, H160, H176, H256, H32, H512, H64, U128, U256, U512, U64,
+    Address, BigEndianHash, Bloom, H128, H160, H176, H256, H32, H456, H512, H64, U128, U256, U456,
+    U512, U64,
 };
 
 pub mod transaction;
